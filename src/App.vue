06e793c9fdc62cc70124921cc9f065432d75fe76
Patrick-web <pntxall100@gmail.com>
2021-11-01 06:29:38 +0300

    Added login requirement

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <login
+      v-if="!isLoggedIn"
+      leave-active-class="animate faster slideOutDown"
+      :msg="adminResponse.error"
+      v-on:adminKey="key => (ADMIN_KEY = key)"
+    />
     <div class="wrapper flex align_start">
       <div class="wrapper m10">
         <div class="sub_fil flex justify_between align_start pa10">
@@ -18,10 +24,14 @@ import SubTotals from "./components/SubTotals.vue";
 import StatsTable from "./components/StatsTable.vue";
 import FilterTitle from "./components/FilterTitle.vue";
 import Filters from "./components/Filters.vue";
+import Login from "./components/Login.vue";
 export default {
   name: "App",
   data() {
     return {
+      ADMIN_KEY: "null",
+      adminResponse: "",
+      isLoggedIn: false,
       stats: [],
       subTotals: {
         currentFilterUsers: 0,
@@ -30,30 +40,49 @@ export default {
       }
     };
   },
+  watch: {
+    ADMIN_KEY() {
+      console.log("Admin Key Changed");
+      this.getStats();
+    }
+  },
   components: {
     SubTotals,
     StatsTable,
     FilterTitle,
-    Filters
+    Filters,
+    Login
   },
   methods: {
     async getStats() {
       console.log("OOhhh yeea");
       const response = await fetch(
-        "https://flb-server.herokuapp.com/get-stats?adminKey=FLBTOTHEMOON"
+        `https://flb-server.herokuapp.com/get-stats?adminKey=${this.ADMIN_KEY}`
       );
-      this.stats = await response.json();
-      console.log(this.stats);
+      console.log(response);
+      if (response.status == 200) {
+        this.stats = await response.json();
+        this.isLoggedIn = true;
+        localStorage.setItem("adminKey", this.ADMIN_KEY);
+      } else {
+        this.adminResponse = await response.json();
+        console.log(this.adminResponse);
+      }
     }
   },
   mounted() {
-    this.getStats();
+    const key = localStorage.getItem("adminKey");
+    if (key) {
+      this.ADMIN_KEY = key;
+      this.getStats();
+    }
   }
 };
 </script>
 
 <style lang="scss">
 @import url("./assets/styles/global.css");
+@import url("./assets/styles/animate.css");
 #app {
   background: white;
   height: 100vh;
---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="login_wrapper">
+    <div class="login">
+      <p>Enter Admin Password</p>
+      <input
+        v-model="key"
+        placeholder="Poor Security all the way"
+        type="password"
+      />
+      <p>{{ msg }}</p>
+      <button @click="$emit('adminKey', key)">LOGIN</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: ""
+    };
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style lang="scss">
+.login_wrapper {
+  position: fixed;
+  z-index: 3;
+  width: 98.5vw;
+  height: 97vh;
+  top: 0px;
+  left: 0px;
+  margin: 10px;
+  background: white;
+  border: 2px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login {
+    width: 300px;
+    padding: 10px;
+    border: 2px solid black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+}
+</style>
